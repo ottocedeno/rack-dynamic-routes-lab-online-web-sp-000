ff1cc1c3dd2b7ddab7aed502274c38c07160c03b
Otto Cedeno <otto@ottostacos.com>
2020-02-19 02:40:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      item == @@items.find {|i| i.name = item_name}
+      item = @@items.find {|i| i.name = item_name}
       binding.pry
 
     else
