1e37c64bf10e1b2dd4c1e1d9f73e4044446a0c64
Otto Cedeno <otto@ottostacos.com>
2020-02-19 02:21:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path
+      item_name = req.path.split("/items").last
       binding.pry
       resp.write "I work"
     else
