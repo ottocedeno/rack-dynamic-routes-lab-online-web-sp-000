88357e928f50a65364b4d1aa40495431419e1c8d
Otto Cedeno <otto@ottostacos.com>
2020-02-19 02:43:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,9 @@ class Application
         resp.write item.price
         resp.status = 200
       end
-      
+
     end
+    
     resp.write "Route not found"
     resp.status = 404
     resp.finish
